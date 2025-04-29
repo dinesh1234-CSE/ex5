f90cd5ffdaee30a660685efd15c0e7f8be2f5920
dinesh1234-CSE <sivadinesh215@gmail.com>
2025-04-29 19:09:47 +0530

    Success

--- leela/leela/wsgi.py
+++ leela/leela/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dinesh project.
+WSGI config for leela project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dinesh.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'leela.settings')
 
 application = get_wsgi_application()
